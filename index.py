c9cafc772fb7dc60c1ef6a32b4bdbd9d530bce49
Long Nguyen <ntl870@gmail.com>
2020-10-26 13:19:39 +0700

    Add normalize and min max

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 # ---------------------------------------BAI TAP NHOM --------------------------------------------------
 
-#'/media/ntl2000/Data/Study/XLTH/Python/BTL/Resources/TinHieuMau/lab_female.wav'
+# '/media/ntl2000/Data/Study/XLTH/Python/BTL/Resources/TinHieuMau/lab_female.wav'
 
 # -------------------------------------LIBRARY-----------------------------------------------------
 import numpy as np
@@ -20,7 +20,7 @@ def chiakhung(Fs, data):  # khoang chia 20ms
     x = int(0.02*Fs)
     for n in range(len(E)):
         while ((n*x + n1) < len(data)):
-            E[n] += abs((data[n*x + n1]*data[n*x + n1]))
+            E[n] += data[n*x + n1]**2
             n1 += 1
             if (n1 == x):
                 n1 = 0
@@ -28,32 +28,49 @@ def chiakhung(Fs, data):  # khoang chia 20ms
     return E
 
 
-def Normalized1(energy,min,max):  # Normalize method 1
-    for i in range(0,len(energy)):               #For loop to normalize LIST energy
+def Normalized1(energy, min, max):  # Normalize method 1
+    for i in range(0, len(energy)):  # For loop to normalize LIST energy
         energy[i] = (energy[i]-min)/(max-min)
 
 
-
- 
-
+def GetThreshold(E):
+    E1 = np.sort(E)
+    max = maxl = 0
+    for i in range(len(E1)-1):
+        if (E1[i+1]-E1[i]) > max:
+            max = E1[i+1]-E1[i]
+            maxl = E1[i]
+    return maxl
 
 
 # --------------------------------------------------MAIN------------------------------------------------------
-Fs, data = read('/media/ntl2000/Data/Study/XLTH/Python/BTL/Resources/TinHieuMau/lab_female.wav')
+Fs, data = read('/media/ntl2000/Data/Study/XLTH/Python/BTL/Resources/TinHieuMau/lab_male.wav')
 print(data)
-arrayX = []
+
 
 E = chiakhung(Fs, data)
 
-#------Normalize E---------
+# ------Normalize E---------
+
 min = min(E)
 max = max(E)
-Normalized1(E,min,max)
-#--------------------
+Normalized1(E, min, max)
+# --------------------
+# Tim nguong
+# E1 = []  # khoi tao mang E
+# for i in range(0, len(E)):
+#     if E[i] <= GetThreshold(E):
+#         E1.append(0)
+#     else:
+#         E1.append(E[i])
 
-print(E)
+
 plt.figure()
+plt.subplot(2, 1, 1)
 plt.plot(E)
+plt.subplot(2, 1, 2)
+plt.plot(data, color="r")
+
 plt.xlabel('sample index')
 plt.ylabel('amplitude')
 plt.title('waveform of test audio')
