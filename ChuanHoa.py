1d10bd47211af788524544bb526c9f379d3ae678
Long Nguyen <ntl870@gmail.com>
2020-10-26 14:10:51 +0700

    Add normalize and min max

--- ChuanHoa.py
+++ ChuanHoa.py
@@ -1,6 +1,6 @@
 import math
 
-
-def ChuanHoa1(energy,min,max):   #Cach 1
+def Normalized1(energy,min,max):   #Cach 1
     res = (energy-min)/(max-min)
     return res
+
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+E1 = []  # khoi tao mang E
+# for i in range(0, len(E)):
+#     if E[i] <= GetThreshold(E):
+#         E1.append(0)
+#     else:
+#         E1.append(E[i])
