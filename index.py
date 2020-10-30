8bb1ee2ceef3ac77bfb691f2fa1983510306855c
Long Nguyen <ntl870@gmail.com>
2020-10-30 16:09:07 +0700

    Add GetEdge with Threshold of 0.01

--- index.py
+++ index.py
@@ -14,7 +14,7 @@ from scipy.io.wavfile import read, write
 def chiakhung(Fs, data):  # khoang chia 20ms
     dur = len(data) / Fs  # thoi gian tin hieu am thanh
     t = np.arange(0, dur, 0.02)  # chia khoang bien do 20ms
-    E = np.zeros(len(t),dtype=np.float64)  # khoi tao mang E
+    E = np.zeros(len(t), dtype=np.float64)  # khoi tao mang E
     n1 = 0
     # xac dinh tuong doi 1 khoang chia gap bao nhieu thoi gian lay mau ( = 0.02/T)
     x = int(0.02*Fs)
@@ -33,7 +33,7 @@ def Normalized1(energy, min, max):  # Normalize method 1
         energy[i] = (energy[i]-min)/(max-min)
 
 
-def GetThreshold(E):
+def GetThreshold(E):    #Get Threshold of E
     E1 = np.sort(E)
     max = maxl = 0
     for i in range(len(E1)-1):
@@ -43,8 +43,24 @@ def GetThreshold(E):
     return maxl
 
 
+def GetEdge(E):         # Get the edges
+    voiced = np.zeros(len(E), dtype=np.float64)
+    unvoiced = np.zeros(len(E), dtype=np.float64)
+    res = np.zeros(len(E), dtype=np.float64)
+    for i in range(0, len(E)-1):
+        if(E[i] > 0.01):
+            voiced[i] = 1
+        else:
+            unvoiced[i] = 1
+
+    for i in range(1, len(E)-1):
+        if voiced[i] == unvoiced[i-1]:
+            res[i-1] = 1
+    return res
+
+
 # --------------------------------------------------MAIN------------------------------------------------------
-Fs, data = read('/media/ntl2000/Data/Study/XLTH/Python/BTL/Resources/TinHieuMau/studio_female.wav')
+Fs, data = read('/media/ntl2000/Data/Study/XLTH/Python/BTL/Resources/TinHieuMau/lab_female.wav')
 print(data)
 
 
@@ -57,26 +73,22 @@ max = max(E)
 Normalized1(E, min, max)
 
 
-# for i in range(0,len(E)):
-#     if E[i] == 0:
-#         print(i)
 
-# --------------------
+E1 = np.zeros(len(E), dtype=np.float64)
 
-# Tim nguong
-    # E1 = []  # khoi tao mang E
-    # for i in range(0, len(E)):
-    #     if E[i] <= GetThreshold(E):
-    #         E1.append(0)
-    #     else:
-    #         E1.append(E[i])
+for i in range(0, len(E1)-1):
+    E1[i] = E[len(E)-i-1]
+ # khoi tao mang E
+x = np.zeros(len(E), dtype=np.float64)
 
+
+arrayX = np.linspace(0, len(E), len(E))
 
 plt.figure()
-plt.subplot(2, 1, 1)
-plt.plot(E)
-plt.subplot(2, 1, 2)
-plt.plot(data, color="r")
+
+plt.plot(arrayX, E, color="r")
+plt.stem(arrayX, GetEdge(E))
+
 
 plt.xlabel('sample index')
 plt.ylabel('amplitude')
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,1 @@
-E1 = []  # khoi tao mang E
-# for i in range(0, len(E)):
-#     if E[i] <= GetThreshold(E):
-#         E1.append(0)
-#     else:
-#         E1.append(E[i])
+plt.subplot(2, 1, 1)
