96d330774584ca459246097fd0cc6871d485bf69
Long Nguyen <ntl870@gmail.com>
2020-10-31 20:03:05 +0700

    Add edges

--- index.py
+++ index.py
@@ -33,6 +33,7 @@ def Normalized1(energy, min, max):  # Normalize method 1
         energy[i] = (energy[i]-min)/(max-min)
 
 
+
 def GetThreshold(E):    #Get Threshold of E
     E1 = np.sort(E)
     max = maxl = 0
@@ -46,7 +47,7 @@ def GetThreshold(E):    #Get Threshold of E
 def GetEdge(E):         # Get the edges
     voiced = np.zeros(len(E), dtype=np.float64)
     unvoiced = np.zeros(len(E), dtype=np.float64)
-    res = np.zeros(len(E), dtype=np.float64)
+    res = []
     for i in range(0, len(E)-1):
         if(E[i] > 0.01):
             voiced[i] = 1
@@ -55,14 +56,15 @@ def GetEdge(E):         # Get the edges
 
     for i in range(1, len(E)-1):
         if voiced[i] == unvoiced[i-1]:
-            res[i-1] = 1
+            res.append(i-1)
     return res
 
 
 # --------------------------------------------------MAIN------------------------------------------------------
-Fs, data = read('/media/ntl2000/Data/Study/XLTH/Python/BTL/Resources/TinHieuMau/lab_female.wav')
-print(data)
+Fs, data = read('/media/ntl2000/Data/Study/XLTH/Python/BTL/Resources/TinHieuMau/phone_female.wav')
 
+mindata = min(data)
+maxdata = max(data)
 
 E = chiakhung(Fs, data)
 
@@ -73,11 +75,6 @@ max = max(E)
 Normalized1(E, min, max)
 
 
-
-E1 = np.zeros(len(E), dtype=np.float64)
-
-for i in range(0, len(E1)-1):
-    E1[i] = E[len(E)-i-1]
  # khoi tao mang E
 x = np.zeros(len(E), dtype=np.float64)
 
@@ -86,11 +83,31 @@ arrayX = np.linspace(0, len(E), len(E))
 
 plt.figure()
 
+plt.subplot(2,1,1)
 plt.plot(arrayX, E, color="r")
-plt.stem(arrayX, GetEdge(E))
+plt.vlines(GetEdge(E),-1,1)
+
+realEdges = []
+
+# Tim cac bien do bo vao mang realEdge
+for i in range(0,len(GetEdge(E))):
+    realEdges.append(GetEdge(E)[i]/0.02)
+print(realEdges)
+#-------------------------------------
+
+lenData = []
+for i in range(0,len(data)):
+    lenData.append(i)
+
+plt.subplot(2,1,2)
+plt.plot(lenData,data, color="r")
+plt.vlines(realEdges,mindata,maxdata)
 
 
 plt.xlabel('sample index')
 plt.ylabel('amplitude')
 plt.title('waveform of test audio')
 plt.show()
+
+
+print(GetEdge(E))
