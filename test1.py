747262cbbe3de3be2fe679be9522739c80679ce7
ntl870 <ntlong870@gmail.com>
2020-11-23 20:35:25 +0700

    updated

--- test1.py
+++ test1.py
@@ -14,6 +14,12 @@ import math
 # # ----------------------------------------------FUNCTION-------------------------------------------
 # # chuyen audio sang tan so lay mau Fs2
 
+def dau(x):
+    if (x > 0):
+        return 1
+    if (x < 0):
+        return -1
+
 
 def chiakhung(Fs, data):  # khoang chia 20ms
     dur = len(data) / Fs  # thoi gian tin hieu am thanh
@@ -38,6 +44,7 @@ def Normalized1(energy, min, max):
         res.append((energy[i]-min)/(max-min))
     return res
 
+
 def ZCRframe(Fs, data):  # khoang chia 20ms
     dur = len(data) / Fs  # thoi gian tin hieu am thanh
     t = np.arange(0, dur, 0.02)  # chia khoang bien do 20ms
@@ -47,13 +54,14 @@ def ZCRframe(Fs, data):  # khoang chia 20ms
     x = int(0.02*Fs)
     for n in range(len(ZCR)):
         while ((n*x + n1) < len(data)-1):
-            if (dau(data[n*x + n1]) != dau(data[n*x + n1 + 1])):
+            if (data[n*x + n1] * data[n*x + n1 + 1] < 0):
                 ZCR[n] += 1
             n1 += 1
             if (n1 == x):
                 n1 = 0
                 break
     return ZCR
+
 
 def CalculateMA(Fs, data):  # khoang chia 20ms
     dur = len(data) / Fs  # thoi gian tin hieu am thanh
@@ -72,7 +80,6 @@ def CalculateMA(Fs, data):  # khoang chia 20ms
     return E
 
 
-
 # def GetEdge(E):         # Get the edge
 #     voiced = np.zeros(len(E), dtype=np.float64)
 #     unvoiced = np.zeros(len(E), dtype=np.float64)
@@ -88,7 +95,7 @@ def CalculateMA(Fs, data):  # khoang chia 20ms
 #             res.append(i-1)
 #     return res
 
-def GetEdgeZCR(E):         # Get the edge
+def GetEdges(E):         # Get the edge
     res = []
     for i in range(0, len(E)-1):
         if(E[i+1] > 0.0 and E[i] == 0.0):
@@ -99,31 +106,58 @@ def GetEdgeZCR(E):         # Get the edge
     u = 0
     while(u < len(res)):
         temp = res[u]
-        for k in range(1,13):
+        for k in range(1, 13):
             if temp + k in res:
                 res.remove(temp + k)
         u += 1
+
+    temp = []
+    for i in range(0, len(res)):
+        for k in range(1, 10):
+            if E[res[i] + k] != 0 and E[res[i] - k] != 0:
+                temp.append(res[i])
+
+    temp = list(dict.fromkeys(temp))
+
+    for i in range(0, len(temp)):
+        res.remove(temp[i])
+
     return res
 
-def GetEdgeMA(E):         # Get the edge
-    res = []
-    for i in range(0, len(E)-1):
-       if(E[i+1] > 0.015 and E[i] < 0.015):
-            res.append(i)
-       elif(E[i-1] > 0.015 and E[i] < 0.015):
-            res.append(i)
 
-    u = 0
-    while(u < len(res)):
-        temp = res[u]
-        for k in range(1,10):
-            if temp + k in res:
-                res.remove(temp + k)
-        u += 1
-    return res
+# def GetEdgeMA(E):         # Get the edge
+#     res = []
+#     zcr = ZCRframe(Fs, data)
+#     for i in range(0, len(E)-1):
+#         if(E[i+1] > 0.15 and E[i] < 0.15):
+#             res.append(i)
+#         elif(E[i-1] > 0.15 and E[i] < 0.15):
+#             res.append(i)
+
+#     u = 0
+#     while(u < len(res)):
+#         temp = res[u]
+#         for k in range(1, 8):
+#             if temp + k in res:
+#                 res.remove(temp + k)
+#         u += 1
+
+#     temp = []
+#     for i in range(0, len(res)):
+#             if E[res[i] + 6] < 0.15 and E[res[i] - 6] > 0.15:
+#                 temp.append(res[i])
+#             elif E[res[i] + 6] > 0.15 and E[res[i] - 6] < 0.15:
+#                 temp.append(res[i])
+
+#     temp = list(dict.fromkeys(temp))
+
+
+
+#     return temp
+
 
 # # --------------------------------------------------MAIN------------------------------------------------------
-Fs, data = read('./Resources/TinHieuMau/studio_male.wav')
+Fs, data = read('./Resources/TinHieuMau/LA025.wav')
 
 
 avg = 0
@@ -135,82 +169,71 @@ for i in range(len(data)):
     altdata[i] = data[i] - avg
 
 
-# E = chiakhung(Fs, data)
+E = chiakhung(Fs, data)
+E = Normalized1(E, min(E), max(E))
+altE = np.zeros(len(E))
+for i in range(0,len(E)):
+    if E[i] <= 0.01:
+        altE[i] = 0
+    else:
+        altE[i] = E[i]
 
-# ------Normalize E---------
-
-
-# E = Normalized1(E, min(E), max(E))
-
- # khoi tao mang E
-# x = np.zeros(len(E), dtype=np.float64)
-
-
-# def calZCRperFrame(data,size):
-#     if size == 0:
-#         return 0
-#     else:
-#      count = 0
-#      for i in range(0,size):
-#         if(data[i-1]*data[i] < 0):
-#             count += 1
-#      return count
-
-
-# def calZCRallFrames(data):
-#     zcr = []
-#     temp = int(0.02*Fs)
-#     alt = 0
-#     while(temp <= len(data)):
-#         zcr.append(calZCRperFrame(data,temp)-calZCRperFrame(data,alt))
-#         alt = temp
-#         temp = temp + int(0.02*Fs)
-#     return zcr
-
-def dau(x):
-    if (x> 0):
-        return 1
-    if (x< 0):  
-        return -1
 
 
 zcr = ZCRframe(Fs,altdata)
-minzcr = min(zcr)
-maxzcr = max(zcr)
-zcr = Normalized1(zcr, minzcr, maxzcr)
+zcr = Normalized1(zcr, min(zcr), max(zcr))
 
 
-realEdges = []
 
 
-MA = CalculateMA(Fs,data)
-MA = Normalized1(MA,min(MA),max(MA))
+
+MA = CalculateMA(Fs, data)
+MA = Normalized1(MA, min(MA), max(MA))
+altMA = np.zeros(len(MA))
+for i in range(0,len(MA)):
+    if MA[i] <= 0.1:
+        altMA[i] = 0
+    else:
+        altMA[i] = MA[i]
+
+realEdgesE = []
+realEdgesZCR = []
+realEdgesMA = []
+
+for i in range(0, len(GetEdges(altE))):
+    realEdgesE.append(GetEdges(altE)[i]*int(0.02*Fs))
+
+for i in range(0, len(GetEdges(altMA))):
+    realEdgesZCR.append(GetEdges(altMA)[i]*int(0.02*Fs))
+
+for i in range(0, len(GetEdges(zcr))):
+    realEdgesMA.append(GetEdges(zcr)[i]*int(0.02*Fs))
 
 
-# Tim cac bien do bo vao mang realEdge
-for i in range(0, len(GetEdgeZCR(zcr))):
-    realEdges.append(GetEdgeZCR(zcr)[i]*int(0.02*Fs))
-# -------------------------------------
-print(GetEdgeZCR(zcr))
 
 plt.figure()
 
 
-plt.subplot(2, 1, 1)
+plt.subplot(2, 3, 1)
+plt.plot(MA, color="r")
+plt.vlines(GetEdges(altMA), 0, 1)
+plt.subplot(2, 3, 4)
+plt.plot(data, color="r")
+plt.vlines(realEdgesMA, -max(data), max(data))
+
+plt.subplot(2, 3, 2)
+plt.plot(E, color="r")
+plt.vlines(GetEdges(altE), 0, 1)
+plt.subplot(2, 3, 5)
+plt.plot(data, color="r")
+plt.vlines(realEdgesE, -max(data), max(data))
+
+plt.subplot(2, 3, 3)
 plt.plot(zcr, color="r")
-plt.vlines(GetEdgeZCR(zcr), 0, 1)
-
-arrayX = []
-
-for i in range(0, len(data)):
-    arrayX.append(i)
-
-plt.subplot(2, 1, 2)
-plt.plot(arrayX, data, color="r")
-plt.vlines(realEdges, -max(data), max(data))
+plt.vlines(GetEdges(zcr), 0, 1)
+plt.subplot(2, 3, 6)
+plt.plot(data, color="r")
+plt.vlines(realEdgesZCR, -max(data), max(data))
 
 
-plt.xlabel('sample index')
-plt.ylabel('amplitude')
-plt.title('waveform of test audio')
 plt.show()
