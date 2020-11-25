1ac50fb017a7d384e4c230f210c6cb2755afcadd
ntl870 <ntlong870@gmail.com>
2020-11-25 08:58:26 +0700

    updated

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,9 +1,1 @@
-  # temp = []
-    # for i in range(0,len(res)):
-    #  for k in range(1,7):
-    #     if E[res[i] + k] > 0.08 and E[res[i] - k] < 0.08:
-    #         temp.append(res[i])
-    #     elif E[res[i] + k] < 0.08 and E[res[i] -k] > 0.08:
-    #         temp.append(res[i])
-    
-    # temp = list(dict.fromkeys(temp))
+
--- test1.py
+++ test1.py
@@ -80,21 +80,6 @@ def CalculateMA(Fs, data):  # khoang chia 20ms
     return E
 
 
-# def GetEdge(E):         # Get the edge
-#     voiced = np.zeros(len(E), dtype=np.float64)
-#     unvoiced = np.zeros(len(E), dtype=np.float64)
-#     res = []
-#     for i in range(0, len(E)-1):
-#         if(E[i] > 0.0):
-#             voiced[i] = 1
-#         else:
-#             unvoiced[i] = 1
-
-#     for i in range(1, len(E)-1):
-#         if voiced[i] == unvoiced[i-1]:
-#             res.append(i-1)
-#     return res
-
 def GetEdges(E):         # Get the edge
     res = []
     for i in range(0, len(E)-1):
@@ -113,10 +98,11 @@ def GetEdges(E):         # Get the edge
 
     temp = []
     for i in range(0, len(res)):
-        for k in range(1, 10):
-            if E[res[i] + k] != 0 and E[res[i] - k] != 0:
+        for k in range(1, 13):
+          if(res[i] - k > 1 and res[i] + k < len(E) - 1):
+            if E[res[i] + k] > 0 and E[res[i] - k] > 0:
                 temp.append(res[i])
-
+    
     temp = list(dict.fromkeys(temp))
 
     for i in range(0, len(temp)):
@@ -125,39 +111,10 @@ def GetEdges(E):         # Get the edge
     return res
 
 
-# def GetEdgeMA(E):         # Get the edge
-#     res = []
-#     zcr = ZCRframe(Fs, data)
-#     for i in range(0, len(E)-1):
-#         if(E[i+1] > 0.15 and E[i] < 0.15):
-#             res.append(i)
-#         elif(E[i-1] > 0.15 and E[i] < 0.15):
-#             res.append(i)
-
-#     u = 0
-#     while(u < len(res)):
-#         temp = res[u]
-#         for k in range(1, 8):
-#             if temp + k in res:
-#                 res.remove(temp + k)
-#         u += 1
-
-#     temp = []
-#     for i in range(0, len(res)):
-#             if E[res[i] + 6] < 0.15 and E[res[i] - 6] > 0.15:
-#                 temp.append(res[i])
-#             elif E[res[i] + 6] > 0.15 and E[res[i] - 6] < 0.15:
-#                 temp.append(res[i])
-
-#     temp = list(dict.fromkeys(temp))
-
-
-
-#     return temp
 
 
 # # --------------------------------------------------MAIN------------------------------------------------------
-Fs, data = read('./Resources/TinHieuMau/lab_male.wav')
+Fs, data = read('./Resources/TinHieuMau/LA025.wav')
 
 
 avg = 0
@@ -173,18 +130,13 @@ E = CalculateSTE(Fs, data)
 E = Normalize(E, min(E), max(E))
 altE = np.zeros(len(E))
 for i in range(0,len(E)):
-    if E[i] <= 0.01:
+    if E[i] <= 0.02:
         altE[i] = 0
     else:
         altE[i] = E[i]
 
-
-
 zcr = CalculateZCR(Fs,altdata)
 zcr = Normalize(zcr, min(zcr), max(zcr))
-
-
-
 
 
 MA = CalculateMA(Fs, data)
@@ -204,36 +156,54 @@ for i in range(0, len(GetEdges(altE))):
     realEdgesE.append(GetEdges(altE)[i]*int(0.02*Fs))
 
 for i in range(0, len(GetEdges(altMA))):
-    realEdgesZCR.append(GetEdges(altMA)[i]*int(0.02*Fs))
+    realEdgesMA.append(GetEdges(altMA)[i]*int(0.02*Fs))
 
 for i in range(0, len(GetEdges(zcr))):
-    realEdgesMA.append(GetEdges(zcr)[i]*int(0.02*Fs))
+    realEdgesZCR.append(GetEdges(zcr)[i]*int(0.02*Fs))
+
+print(GetEdges(zcr))
+print(GetEdges(altMA))
+print(GetEdges(altE))
 
 
 
 plt.figure()
 
+k = [29000,133300,142900,198800,211600,254200,264300,323900]
+plt.title("File LA025")
+plt.plot(data,color="r")
+plt.vlines(k, -max(data), max(data))
 
-plt.subplot(2, 3, 1)
-plt.plot(MA, color="r")
-plt.vlines(GetEdges(altMA), 0, 1)
-plt.subplot(2, 3, 4)
-plt.plot(data, color="r")
-plt.vlines(realEdgesMA, -max(data), max(data))
 
-plt.subplot(2, 3, 2)
-plt.plot(E, color="r")
-plt.vlines(GetEdges(altE), 0, 1)
-plt.subplot(2, 3, 5)
-plt.plot(data, color="r")
-plt.vlines(realEdgesE, -max(data), max(data))
 
-plt.subplot(2, 3, 3)
-plt.plot(zcr, color="r")
-plt.vlines(GetEdges(zcr), 0, 1)
-plt.subplot(2, 3, 6)
-plt.plot(data, color="r")
-plt.vlines(realEdgesZCR, -max(data), max(data))
+
+
+# plt.subplot(2, 3, 1)
+# plt.plot(MA, color="r")
+# plt.title("MA")
+# plt.vlines(GetEdges(altMA), 0, 1)
+# plt.subplot(2, 3, 4)
+# plt.title("Data MA")
+# plt.plot(data, color="r")
+# plt.vlines(realEdgesMA, -max(data), max(data))
+
+# plt.subplot(2, 3, 2)
+# plt.plot(E, color="r")
+# plt.title("E")
+# plt.vlines(GetEdges(altE), 0, 1)
+# plt.subplot(2, 3, 5)
+# plt.title("Data STE")
+# plt.plot(data, color="r")
+# plt.vlines(realEdgesE, -max(data), max(data))
+
+# plt.subplot(2, 3, 3)
+# plt.plot(zcr, color="r")
+# plt.title("ZCR")
+# plt.vlines(GetEdges(zcr), 0, 1)
+# plt.subplot(2, 3, 6)
+# plt.title("Data ZCR")
+# plt.plot(data, color="r")
+# plt.vlines(realEdgesZCR, -max(data), max(data))
 
 
 plt.show()
